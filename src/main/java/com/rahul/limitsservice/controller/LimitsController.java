d02c37ddbc2389ec97add1b082d8ae5518d8bc51
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-07-21 17:42:01 +0530

    using @ConfigurationProperties

--- src/main/java/com/rahul/limitsservice/controller/LimitsController.java
+++ src/main/java/com/rahul/limitsservice/controller/LimitsController.java
@@ -1,5 +1,6 @@
 package com.rahul.limitsservice.controller;
 
+import com.rahul.limitsservice.configuration.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,16 @@ import com.rahul.limitsservice.bean.Limits;
 @RestController
 @RequestMapping("/api")
 public class LimitsController {
-	@GetMapping("/limits")
-	public Limits retrievLimits() {
-		return new Limits(1,1000);
-	}
+    private Configuration configuration;
+
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @GetMapping("/limits")
+    public Limits retrievLimits() {
+        return new Limits(configuration.getMinimum(),configuration.getMaximum());
+        //return new Limits(1, 1000);
+    }
 
 }
